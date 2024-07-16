b6f94e621d20fd73a37e8afa192915b372128d2b
subodh245 <kamblesubodh189@gamil.com>
2024-07-16 13:10:55 +0530

    fix type script error in page.tsx

--- components/ui/Header.tsx
+++ components/ui/Header.tsx
@@ -1,13 +1,13 @@
 interface Props {
-  type: string;
+
   query: string;
   category: string;
 }
-const Header = ({ type, query, category }: Props) => {
+const Header = ({ query, category }: Props) => {
   if (query && category) {
     return (
       <h1 className="heading3 self-start text-white-800">
-        Search results for "{query}" in {type}
+        Search results for "{query}" in
         <span className="capitalize">{category}</span>
       </h1>
     );
@@ -15,7 +15,7 @@ const Header = ({ type, query, category }: Props) => {
   if (query) {
     return (
       <h1 className="heading3 self-start text-white-800">
-        Search results for "{query}" in {type}
+        Search results for "{query}" in
       </h1>
     );
   }
